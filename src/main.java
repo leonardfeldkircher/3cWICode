a1520cfd9a3423b1779bf1a9eafcfaa41573222d
leonard <you@example.com>
2022-09-20 16:06:04 +0200

    created new files

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+        public static void main(String[] args) {
+
+            int number = 7 ;
+            String name = "Andi";
+
+            System.out.println(name);
+            System.out.println(number);
+        }
+}
