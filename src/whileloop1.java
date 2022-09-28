4c21f750f55a467fc95e8f98602e0bd48b0f22b4
leonard <you@example.com>
2022-09-28 12:00:43 +0200

    Finished loop uebung 1 and 2

---
+++ src/whileloop1.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class whileloop1 {
+    public static void main(String[] args){
+
+        Random random = new Random();
+        boolean isFinished = false;
+
+        int a = 0;
+        while (!isFinished){
+            int randomValue = random.nextInt(10,30);
+            System.out.println("Nr: " + randomValue);
+
+            if (randomValue==15 || randomValue==25){
+                isFinished=true;
+            }
+            else {
+                a = a + randomValue;
+            }
+        }
+        System.out.println(a);
+    }
+}
