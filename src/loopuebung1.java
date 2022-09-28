dde590f42eca0a783923b72495efb5de7b81c7a2
leonard <you@example.com>
2022-09-28 11:34:14 +0200

    Finished loop uebung 1 and 2

---
+++ src/loopuebung1.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class loopuebung1 {
+    public static void main(String[] args){
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(10);
+
+        int a = 0;
+        for (int i = 1; i <= 100; i++) {
+            a = a + i;
+            System.out.println(i);
+
+        }
+        System.out.println(a);
+    }
+}
