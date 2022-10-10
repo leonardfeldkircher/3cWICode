135f57e0a0bc884ad9c19d22557b8e62ef55301f
leonard <you@example.com>
2022-10-10 12:17:26 +0200

    finished cubegame

--- src/cubegame.java
+++ src/cubegame.java
@@ -4,64 +4,70 @@ import java.util.Random;
 public class cubegame {
 
     public static void main(String[] args) {
-        int randomNumberOfComputer = 0;
 
-        int amountOfWinsComputer = 0;
-        int amountOfWinsPlayer = 0;
+        int randomNumber1 = 0;
+        int randomNumber2 = 0;
+        int randomNumber3 = 0;
+        int randomNumber4 = 0;
+        int randomNumber5 = 0;
+        int randomNumber6 = 0;
+
         boolean cubegame = false;
         System.out.println("Drücken sie die 1 für ihren ersten wurf");
 
         Random random = new Random();
         int randomNumber = random.nextInt(6, 36);
 
-        while(cubegame==false) {
+        while (cubegame == false) {
 
-            //System.out.println(randomNumber);
             Scanner scanner = new Scanner(System.in);
             int selection = scanner.nextInt();
 
             if (selection == 1) {
-                randomNumberOfComputer = random.nextInt(1, 6);
-                // player
-
-                System.out.println("Sie haben " + randomNumberOfComputer + " gewürfelt!");
+                randomNumber1 = random.nextInt(1, 6);
+                System.out.println("Sie haben " + randomNumber1 + " gewürfelt!");
                 System.out.println("Drücken sie die 2 für ihren zweiten wurf");
-            }
-
-            else if (selection == 2) {
-                 randomNumberOfComputer = random.nextInt(1, 6);
-                System.out.println("Sie haben " + randomNumberOfComputer + " gewürfelt!");
+            } else if (selection == 2) {
+                randomNumber2 = random.nextInt(1, 6);
+                System.out.println("Sie haben " + randomNumber2 + " gewürfelt!");
                 System.out.println("Drücken sie die 3 für ihren dritten wurf");
-            }
-
-            else if (selection == 3) {
-                int randomNumber1 = random.nextInt(1, 6);
-                System.out.println("Sie haben " + randomNumber1 + " gewürfelt!");
+            } else if (selection == 3) {
+                randomNumber3 = random.nextInt(1, 6);
+                System.out.println("Sie haben " + randomNumber3 + " gewürfelt!");
                 System.out.println("Drücken sie die 4 für ihren vierten wurf");
-            }
-
-            else if (selection == 4) {
-                int randomNumber1 = random.nextInt(1, 6);
-                System.out.println("Sie haben " + randomNumber1 + " gewürfelt!");
+            } else if (selection == 4) {
+                randomNumber4 = random.nextInt(1, 6);
+                System.out.println("Sie haben " + randomNumber4 + " gewürfelt!");
                 System.out.println("Drücken sie die 5 für ihren fünften wurf");
-            }
-
-            else if (selection == 5) {
-                int randomNumber1 = random.nextInt(1, 6);
-                System.out.println("Sie haben " + randomNumber1 + " gewürfelt!");
+            } else if (selection == 5) {
+                randomNumber5 = random.nextInt(1, 6);
+                System.out.println("Sie haben " + randomNumber5 + " gewürfelt!");
                 System.out.println("Drücken sie die 6 für ihren sechsten wurf");
-            }
+            } else if (selection == 6) {
+                randomNumber6 = random.nextInt(1, 6);
+                System.out.println("Sie haben " + randomNumber6 + " gewürfelt!");
 
-            else if (selection == 6) {
-                int randomNumber1 = random.nextInt(1, 6);
-                System.out.println("Sie haben " + randomNumber1 + " gewürfelt!");
-                //System.out.println("Drücken sie die 6 für ihren sechsten wurf");
-                System.out.println("Computer: " + randomNumber);
+                int a = randomNumber1;
+                int b = randomNumber2;
+                int c = randomNumber3;
+                int d = randomNumber4;
+                int e = randomNumber5;
+                int f = randomNumber6;
+
+                System.out.println("Augenzahl des Computers: " + randomNumber);
+                System.out.println("Ihre Augenzahl: " + (a + b + c + d + e + f));
+
+                if (randomNumber>(a+b+c+d+e+f)) {
+                    System.out.println("Sie haben Verloren");
+                }
+                else if (randomNumber==(a+b+c+d+e+f)){
+                    System.out.println("Unentschieden!");
+                }
+                else {
+                    System.out.println("Sie haben Gewonnen");
+                    cubegame = true;
+                }
             }
         }
-
-
-        //a = a + randomNumber1;
-
     }
 }
