5ee125f6626ddcc6958620c8c8ad3344a4cddb74
leonard <you@example.com>
2022-10-13 09:25:52 +0200

    started tictactoe

---
+++ src/tictactoe.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class tictactoe {
+
+    public static void main(String[] args){
+
+        int[][] playGround = new int[3][3];
+
+        playGround[0][0] = 1;
+
+        for (int row = 0; row < playGround.length; row++) {
+            for (int col = 0; col < playGround.length; col++) {
+                System.out.print(playGround[row][col] + " ");
+            }
+            System.out.println();
+        }
+
+        boolean ticTacToe = false;
+        System.out.println("Spieler 1 beginnt!");
+
+
+        while (ticTacToe == false) {
+
+            Scanner scanner = new Scanner(System.in);
+            int selection = scanner.nextInt();
+
+        }
+    }
+}
