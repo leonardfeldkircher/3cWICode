c92a2ecb669b27fcfabdf8a8735871db3161c7f8
leonard <you@example.com>
2022-11-10 10:12:59 +0100

    worked on tictactoe

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -6,28 +6,38 @@ public class tictactoe {
     public static void main(String[] args){
 
         int[][] playGround = new int[3][3];
+        Scanner scanner = new Scanner(System.in);
+        boolean isPlayerOne = true;
 
-        playGround[0][0] = 1;
+        printField(playGround);
 
+        while(true){
+            String inputOfUser = scanner.next();
+
+            String[] arr = inputOfUser.split(",");
+            System.out.println(arr);
+
+            int x = Integer.valueOf(arr[0]);
+            int y = Integer.valueOf(arr[1]);
+            playGround[x][y] = 1;
+
+            printField(playGround);
+
+            if (isPlayerOne){
+                isPlayerOne = false;
+            } else {
+                isPlayerOne = true;
+            }
+            System.out.println(isPlayerOne);
+        }
+    }
+
+    public static void printField(int[][] playGround){
         for (int row = 0; row < playGround.length; row++) {
             for (int col = 0; col < playGround.length; col++) {
                 System.out.print(playGround[row][col] + " ");
             }
             System.out.println();
-        }
-
-        boolean ticTacToe = false;
-        System.out.println("Spieler 1 beginnt!");
-
-
-        while (ticTacToe == false) {
-
-            Scanner scanner = new Scanner(System.in);
-            int selection = scanner.nextInt();
-
-            //if (selection == ("0,0")) {
-                //playGround.length = row;
-            }
         }
     }
 }
